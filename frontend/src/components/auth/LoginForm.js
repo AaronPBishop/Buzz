84ee0fccd04c0593e490a8fa2b9bb85a67bb3366
Alain Alvarez <alainalvarez14@gmail.com>
2023-01-06 14:40:34 -0500

    added styling to signup form and login form

--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -29,33 +29,90 @@ const LoginForm = () => {
   if (user) return <Redirect to='/home' />;
 
   return (
-    <form onSubmit={onLogin}>
+    <form onSubmit={onLogin}
+      style={{
+        border: '2px rgb(240, 210, 10) solid',
+        borderRadius: '20px',
+        display: 'flex',
+        flexDirection: 'column',
+        marginTop: '20vh',
+        height: '40vh',
+        justifyContent: 'center'
+        // width: '20vw',
+      }}>
+      <div
+        style={{
+          color: 'rgb(240, 210, 10)',
+          fontSize: '20px',
+          display: 'flex',
+          justifyContent: 'space-between',
+          // marginBottom: '30px',
+          margin: '0 auto 30px auto',
+        }}>
+        Log In
+      </div>
       <div>
         {errors.map((error, ind) => (
           <div key={ind}>{error}</div>
         ))}
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          padding: '0 10px 10px'
+        }}>
+        <label htmlFor='email'
+          style={{
+            color: 'yellow',
+
+          }}></label>
         <input
           name='email'
           type='text'
           placeholder='Email'
           value={email}
           onChange={updateEmail}
+          style={{
+            width: '200px',
+            height: '20px',
+            borderRadius: '8px',
+          }}
         />
       </div>
-      <div>
-        <label htmlFor='password'>Password</label>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          padding: '0 10px 10px'
+        }}>
+        <label htmlFor='password'
+          style={{
+            color: 'yellow'
+          }}></label>
         <input
           name='password'
           type='password'
           placeholder='Password'
           value={password}
           onChange={updatePassword}
+          style={{
+            width: '200px',
+            height: '20px',
+            borderRadius: '8px'
+          }}
         />
-        <button type='submit'>Login</button>
       </div>
+      <button type='submit'
+        style={{
+          borderRadius: '8px',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          height: '30px',
+          width: '150px',
+          background: 'rgb(240, 210, 10)',
+          cursor: 'pointer'
+        }}>Login</button>
     </form>
   );
 };
--- frontend/src/components/auth/SignUpForm.js
+++ frontend/src/components/auth/SignUpForm.js
@@ -30,75 +30,190 @@ const SignUpForm = () => {
   if (user) return <Redirect to='/home' />;
 
   return (
-    <form onSubmit={onSignUp}>
+    <form onSubmit={onSignUp}
+      style={{
+        border: '2px rgb(240, 210, 10) solid',
+        borderRadius: '20px',
+        display: 'flex',
+        flexDirection: 'column',
+        marginTop: '20vh',
+        height: '40vh',
+        justifyContent: 'center'
+        // width: '20vw',
+      }}>
+      <div
+        style={{
+          color: 'rgb(240, 210, 10)',
+          fontSize: '20px',
+          display: 'flex',
+          justifyContent: 'space-between',
+          // marginBottom: '30px',
+          margin: '0 auto 30px auto',
+        }}>
+        Sign Up
+      </div>
       <div>
         {errors.map((error, ind) => (
           <div key={ind}>{error}</div>
         ))}
       </div>
 
-      <div>
-        <label>User Name</label>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          padding: '0 10px 10px'
+        }}>
+        <label
+          style={{
+            color: 'yellow',
+
+          }}></label>
         <input
           type='text'
           name='username'
           onChange={e => setUsername(e.target.value)}
+          placeholder='Username'
           value={username}
+          style={{
+            width: '200px',
+            height: '20px',
+            borderRadius: '8px',
+          }}
         ></input>
       </div>
 
-      <div>
-        <label>First Name</label>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          padding: '0 10px 10px'
+        }}>
+        <label
+          style={{
+            color: 'yellow'
+          }}></label>
         <input
           type='text'
           name='firstName'
           onChange={e => setFirstName(e.target.value)}
+          placeholder='First Name'
           value={firstName}
+          style={{
+            width: '200px',
+            height: '20px',
+            borderRadius: '8px'
+          }}
         ></input>
       </div>
 
-      <div>
-        <label>Last Name</label>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          padding: '0 10px 10px'
+        }}>
+        <label
+          style={{
+            color: 'yellow'
+          }}></label>
         <input
           type='text'
           name='lastName'
           onChange={e => setLastName(e.target.value)}
+          placeholder='Last Name'
           value={lastName}
+          style={{
+            width: '200px',
+            height: '20px',
+            borderRadius: '8px'
+          }}
         ></input>
       </div>
 
-      <div>
-        <label>Email</label>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          padding: '0 10px 10px'
+        }}>
+        <label
+          style={{
+            color: 'yellow'
+          }}></label>
         <input
           type='text'
           name='email'
           onChange={e => setEmail(e.target.value)}
+          placeholder='Email'
           value={email}
+          style={{
+            width: '200px',
+            height: '20px',
+            borderRadius: '8px'
+          }}
         ></input>
       </div>
 
-      <div>
-        <label>Password</label>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          padding: '0 10px 10px'
+        }}>
+        <label
+          style={{
+            color: 'yellow'
+          }}></label>
         <input
           type='password'
           name='password'
           onChange={e => setPassword(e.target.value)}
+          placeholder='Password'
           value={password}
+          style={{
+            width: '200px',
+            height: '20px',
+            borderRadius: '8px'
+          }}
         ></input>
       </div>
 
-      <div>
-        <label>Repeat Password</label>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          padding: '0 10px 10px'
+        }}>
+        <label
+          style={{
+            color: 'yellow'
+          }}></label>
         <input
           type='password'
           name='repeat_password'
           onChange={e => setRepeatPassword(e.target.value)}
+          placeholder='Repeat Password'
           value={repeatPassword}
           required={true}
+          style={{
+            width: '200px',
+            height: '20px',
+            borderRadius: '8px'
+          }}
         ></input>
       </div>
 
-      <button type='submit'>Sign Up</button>
+      <button type='submit'
+        style={{
+          borderRadius: '8px',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          height: '30px',
+          width: '150px',
+          background: 'rgb(240, 210, 10)',
+          cursor: 'pointer'
+        }}>Sign Up</button>
     </form>
   );
 };
