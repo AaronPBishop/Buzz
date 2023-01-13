33c03208cb21a1335ce60e7f32a0bef6c1921763
Alain Alvarez <alainalvarez14@gmail.com>
2023-01-13 17:52:01 -0500

    changed logo styling

--- frontend/src/components/Logo/Logo.js
+++ frontend/src/components/Logo/Logo.js
@@ -1,38 +1,40 @@
 const Logo = () => {
 
     return (
-        <div 
-        style={{
-            display: 'flex',
-            justifyContent: 'center',
-            width: '8vw',
-            height: '4.5vh',
-            backgroundColor: 'yellow',
-            marginTop: 'auto',
-            marginBottom: 'auto',
-            marginRight: '20px',
-            marginLeft: '-20vw',
-        }} 
-        className="buzz-btn">
-
-            <img 
-            src={require('./bee.png').default} 
+        <div
             style={{
-                marginTop: '-1.5vh',
-                zIndex: '100',
-                width: '60px',
-                height: '55px',
-            }}>
+                display: 'flex',
+                justifyContent: 'center',
+                width: '8vw',
+                height: '4.5vh',
+                backgroundColor: 'yellow',
+                marginTop: 'auto',
+                marginBottom: 'auto',
+                marginRight: '20px',
+                marginLeft: '-20vw',
+            }}
+            className="buzz-btn">
+
+            <img
+                src={require('./bee.png').default}
+                style={{
+                    // marginTop: '-1.5vh',
+                    position: 'relative',
+                    bottom: '1vh',
+                    zIndex: '100',
+                    width: '60px',
+                    height: '55px',
+                }}>
             </img>
 
-            <div 
-            style={{
-                fontStyle: 'bold',
-                fontSize: '20px',
-                marginTop: '0.1vh',
-                marginRight: '0.5vw',
-                padding: '5px'
-            }}>
+            <div
+                style={{
+                    fontStyle: 'bold',
+                    fontSize: '20px',
+                    marginTop: '0.1vh',
+                    marginRight: '0.5vw',
+                    padding: '5px'
+                }}>
                 Buzz
             </div>
 
